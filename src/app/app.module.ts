9479c349d409371576a06b971a2a90aa9526174a
Dina <sipos.edina02@gmail.com>
2022-03-29 20:54:39 +0200

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './page/home/home.component';
-import { ProductComponent } from './page/product/product.component';
+import { CarsComponent } from './page/cars/cars.component';
+import { CategoryComponent } from './page/category/category.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ProductComponent
+    CarsComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
